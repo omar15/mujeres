6ac6e26cad69df0af61f9528602331c0c72debe3
Moatilliatta <murdoc_87_1@hotmail.com>
2014-08-06 15:30:29 -0500

    Se agrega rango de fecha en reporte

--- estadisticas/mujeres/reporte_caravana.php
+++ estadisticas/mujeres/reporte_caravana.php
@@ -2,7 +2,8 @@
 session_start();//Habilitamos uso de variables de sesión
 
 //Si requerimos obtener totales por fecha
-$fecha_creacion = ($_GET['fecha_creacion'] != NULL)? $_GET['fecha_creacion']: NULL;
+$fecha_creacion_ini = ($_GET['fecha_creacion_ini'] != NULL)? $_GET['fecha_creacion_ini']: NULL;
+$fecha_creacion_fin = ($_GET['fecha_creacion_fin'] != NULL)? $_GET['fecha_creacion_fin']: NULL;
 $excel = ($_POST['excel'] != NULL)? $_POST['excel']: NULL;
 
 //Imprimimos o no cabecera de excel
@@ -23,12 +24,13 @@ if($excel != NULL){
 include_once($_SESSION['model_path'].'mujeres_avanzando.php');
 
 //Obtenemos totales por caravana
-$lista = mujeresAvanzando::repCarGrado($fecha_creacion);
-$tot_car = mujeresAvanzando::totalRepCarGrado($fecha_creacion);
+$lista = mujeresAvanzando::repCarGrado($fecha_creacion_ini,$fecha_creacion_fin);
+$tot_car = mujeresAvanzando::totalRepCarGrado($fecha_creacion_ini,$fecha_creacion_fin);
 
 if($excel == NULL){ ?>
 
 <script lang="JavaScript" type="text/javascript" src="<?php echo $_SESSION['js_path']?>combobox.js"></script>
+<script lang="JavaScript" type="text/javascript" src="<?php echo $_SESSION['js_path']?>fecha.js"></script>
 <script lang="JavaScript" type="text/javascript" src="<?php echo $_SESSION['js_path']?><?php echo $_SESSION['module_name']?>/valida.js"></script>
 <script lang="javascript" type="text/javascript" src="<?php echo $_SESSION['js_path'];?>jquery.tablesorter.min.js"></script>
 <script type="text/javascript">
@@ -46,6 +48,15 @@ $(function() {
     
     <div class="centro">              
     <div>   
+
+    <?php if($excel == NULL){ ?>
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+    <div>
+        <input type="hidden" id="excel" name="excel" value="1" />
+        <input type="submit" value ="Generar Excel"/>
+    </div>        
+    </form> 
+    <?php } ?>
 
     <table id="totales_caravana" class="tablesorter">
     <thead>
@@ -88,25 +99,23 @@ $(function() {
     <form method="GET" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
         <table>
         <tr>
-            <th>Fecha Creaci&oacute;n</th>
-            <td>
-                <input type="text" id="fecha_creacion" class="fecha" name="fecha_creacion"/>
-            </td>
+            <th>Consultar desde Fecha Creaci&oacute;n</th>            
         </tr>
         <tr>
+            <td>
+                <label>Desde</label> 
+                <input type="text" id="from" name="fecha_creacion_ini" value="<?php echo $fecha_creacion_ini; ?>" />
+            </td>
+            <td>
+                <label>Hasta</label> 
+                <input type="text" id="to" name="fecha_creacion_fin" value="<?php echo $fecha_creacion_fin; ?>" />
+            </td>
             <td>&nbsp;</td>
             <td><input type="submit" value ="Filtrar"/></td>
         </tr>
         </table>
-    </form> 
+    </form>     
 
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-    <div>
-        <input type="hidden" id="excel" name="excel" value="1" />
-        <input type="submit" value ="Generar Excel"/>
-    </div>        
-    </form> 
-    
     </div>
  </div>
 </div>
--- js/fecha.js
+++ js/fecha.js
@@ -14,5 +14,33 @@ jQuery(document).ready(function ($) {
       nextText: "Sig",
       yearRange: "1900:1996"  
     });
+
+    $( "#from" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dayNames: [ "Lunes","Martes","Miércoles","Jueves","Viernes","Sabado","Domingo"],
+      dayNamesMin: [ "Lun","Mar","Mie","Jue","Vie","Sab","Dom" ],
+      monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+      monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec" ],
+      dateFormat: "yy-mm-dd",
+      nextText: "Sig",
+        onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+    }
+    });
+
+    $( "#to" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dayNames: [ "Lunes","Martes","Miércoles","Jueves","Viernes","Sabado","Domingo"],
+      dayNamesMin: [ "Lun","Mar","Mie","Jue","Vie","Sab","Dom" ],
+      monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+      monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec" ],
+      dateFormat: "yy-mm-dd",
+      nextText: "Sig",
+        onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+    }
+    });
        
 });
